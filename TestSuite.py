f72619848ddc7b5a06dec46e50d89508875a3d63
D3L1J3 <37899526+D3L1J3@users.noreply.github.com>
2023-01-19 12:37:45 +0100

    commit comment 1

--- TestSuite.py
+++ TestSuite.py
@@ -1,9 +1,11 @@
 import time
 import unittest
+from os import name
 
 from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
+
 
 # This code is a test suite for a Google search page, it includes four test cases:
 # Test1 - test_page_title: It navigates to the Google homepage and verifies that the title of
@@ -22,18 +24,23 @@ from selenium.webdriver.common.by import By
 # clicks on the accept button, clicks on the sign in link,
 # and then asserts that the current URL contains "accounts.google.com/v3/signin/identifier".
 
+# setUp method opens a Firefox browser and navigates to the Google homepage
+# The tearDown method closes the browser after all tests are run.
+# The if statement at the end of the code allows the test suite to be run as the main program.
+
 class GoogleTestCase(unittest.TestCase):
 
+    browser = None
     def setUp(self):
         self.browser = webdriver.Firefox()
-        self.addCleanup(self.browser.quit)
+        self.browser.get("https://www.google.com")
+        options = webdriver.FirefoxOptions()
+        options.add_argument('--disable-popup-blocking')
 
     def test_page_title(self):
-        self.browser.get('http://www.google.com')
         self.assertIn('Google', self.browser.title)
 
     def test_search_for_selenium(self):
-        self.browser.get("https://www.google.com")
         acceptButton = self.browser.find_element(By.CSS_SELECTOR, "button#L2AGLb")
         acceptButton.click()
         search_field = self.browser.find_element(By.NAME, "q")
@@ -54,9 +61,17 @@ class GoogleTestCase(unittest.TestCase):
         self.assertTrue(len(images_list) > 0)
 
     def test_click_on_sign_in_button(self):
-        self.browser.get("https://www.google.com")
         acceptButton = self.browser.find_element(By.CSS_SELECTOR, "button#L2AGLb")
         acceptButton.click()
         sign_in_link = self.browser.find_element(By.CSS_SELECTOR, "a.gb_ha.gb_ia.gb_ee.gb_ed")
         sign_in_link.click()
         self.assertIn("accounts.google.com/v3/signin/identifier", self.browser.current_url)
+
+    def tearDown(self):
+        if self.browser:
+            self.browser.quit()
+
+if name == 'main':
+    unittest.main()
+
+
